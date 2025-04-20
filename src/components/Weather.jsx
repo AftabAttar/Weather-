b0cb592accea543952944cb7ad59c8d0caf4b004
AftabAttar <aftabattar07@gmail.com>
2025-04-20 11:58:27 +0530

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,107 @@
+import { useEffect, useRef, useState } from "react";
+import styles from "./Weather.module.css";
+import Search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import humidity_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+
+export default function Weather() {
+  const inputRef = useRef();
+  const [weatherData, setWeatherData] = useState(null);
+
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+
+  const search = async (city) => {
+    if (city === "") {
+      alert("Enter City Name");
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+      const res = await fetch(url);
+      const data = await res.json();
+
+      if (!res.ok) {
+        alert(data.message);
+        return;
+      }
+      console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      setWeatherData(null);
+      console.error("Error in fetching weather data:", error);
+      alert("Failed to fetch weather data. Please try again.");
+    }
+  };
+
+  useEffect(() => {
+    search("Landon");
+  }, []);
+
+  return (
+    <div className={styles.weather}>
+      <div className={styles.searchBar}>
+        <input ref={inputRef} type="text" placeholder="Search" />
+        <img
+          src={Search_icon}
+          alt="Search"
+          onClick={() => search(inputRef.current.value)}
+        />
+      </div>
+      {weatherData ? (
+        <>
+          <img src={weatherData.icon} alt="" className={styles.weatherIcon} />
+          <p className={styles.temp}>{weatherData.temperature} °C</p>
+          <p className={styles.location}>{weatherData.location}</p>
+          <div className={styles.weatherData}>
+            <div className={styles.weatherInfo}>
+              <img src={humidity_icon} alt="Humidity" />
+              <div>
+                <p>{weatherData.humidity} %</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+
+            <div className={styles.weatherInfo}>
+              <img src={wind_icon} alt="Wind Speed" />
+              <div>
+                <p>{weatherData.windSpeed} Km/h</p>
+                <span>Speed</span>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  );
+}
